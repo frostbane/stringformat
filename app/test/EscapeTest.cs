59cf2c59e6cb17717f18b547fdbef09a835fc3c4
frostbane <frostbane@programmer.net>
2023-09-24 00:59:16 +1000

    add regex special character test for escape sequence

--- app/test/EscapeTest.cs
+++ app/test/EscapeTest.cs
@@ -17,7 +17,7 @@ public class EscapeTest
     [InlineData("?")]
     [InlineData("\\")]
     public void
-    TestRegexEscape(string c)
+    TestRegexEscapeMatch(string c)
     {
         StringFormat sf = new ();
 
@@ -34,6 +34,41 @@ public class EscapeTest
 
         string expected = "select id from t_users;";
         string template = "select " + m + "col" + m + " from " + m + "table" + m +";";
+        string result   = sf.Format(template , map);
+
+        Assert.Equivalent(expected, result, strict: true);
+    }
+
+    [Theory]
+    [InlineData(".")]
+    [InlineData("$")]
+    [InlineData("^")]
+    [InlineData("[")]
+    [InlineData("(")]
+    [InlineData("|")]
+    [InlineData(")")]
+    [InlineData("*")]
+    [InlineData("+")]
+    [InlineData("?")]
+    [InlineData("\\")]
+    public void
+    TestRegexEscapeIgnore(string c)
+    {
+        StringFormat sf = new ();
+
+        string m = c + c;
+
+        sf.SetEscapeStart(m)
+          .SetEscapeEnd(m);
+
+        var map = new Dictionary<string, object>()
+        {
+            { "col", "id" },
+            { "table", "t_users" },
+        };
+
+        string expected = "select {{col}} from t_users;";
+        string template = "select " + m + "{{col}}" + m + " from {{table}};";
         string result   = sf.Format(template , map);
 
         Assert.Equivalent(expected, result, strict: true);
